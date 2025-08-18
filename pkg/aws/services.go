5a320561c12ff7131b90864f73bf2229e1fb0d4f
sbldevnet <sbldevnet@gmail.com>
2025-08-18 10:03:57 +0200

    fix: reduce concurrency and increase backoff to avoid TooManyRequestsException

--- pkg/aws/services.go
+++ pkg/aws/services.go
@@ -1,7 +1,10 @@
 package aws
 
 import (
+	"time"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/retry"
 	"github.com/aws/aws-sdk-go-v2/service/organizations"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
@@ -20,6 +23,13 @@ func newStsClient(awscfg aws.Config) (*StsClient, error) {
 }
 
 func newOrganizationsClient(awscfg aws.Config) (*OrganizationsClient, error) {
+	awscfg.Retryer = func() aws.Retryer {
+		return retry.NewStandard(func(o *retry.StandardOptions) {
+			o.MaxAttempts = 5
+			o.Backoff = retry.NewExponentialJitterBackoff(1 * time.Second)
+		})
+	}
+
 	client := organizations.NewFromConfig(awscfg)
 	return &OrganizationsClient{client: client}, nil
 }
